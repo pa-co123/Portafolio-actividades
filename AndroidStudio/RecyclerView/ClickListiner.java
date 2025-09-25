7d47d98915cbf55d6f9833bd3e05b3a1623b5447
pa-co123 <202260263@ucc.mx>
2025-09-24 23:36:16 -0600

    Archvios recledView

---
+++ AndroidStudio/RecyclerView/ClickListiner.java
@@ -0,0 +1,9 @@
+package com.example.recyclerview;
+
+public abstract class ClickListiner{
+
+    // here index is index
+// of item clicked
+    public abstract void click(int index);
+
+}
---
+++ AndroidStudio/RecyclerView/examData.java
@@ -0,0 +1,14 @@
+package com.example.recyclerview;
+
+public class examData {
+    String name;
+    String date;
+    String message;
+
+    examData(String name,String date,String message)
+    {
+        this.name = name;
+        this.date = date;
+        this.message = message;
+    }
+}
